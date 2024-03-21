d16035f4c677633750c2e720a4040e0724127851
rawanalzidan <rwan14192010@gmail.com>
2024-03-21 20:47:01 +0300

    -add getArtWorks state

--- app/src/main/java/com/example/exhibitionsapp/ui/dashboard/DashboardViewModel.kt
+++ app/src/main/java/com/example/exhibitionsapp/ui/dashboard/DashboardViewModel.kt
@@ -8,12 +8,13 @@ import com.example.exhibitionsapp.common.UIState
 import com.example.exhibitionsapp.data.model.ExhibitionResponse
 import com.example.exhibitionsapp.domain.GetExhibitionsUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class DashboardViewModel @Inject constructor(
-    val getExhibitionsUseCase: GetExhibitionsUseCase,
+    private val getExhibitionsUseCase: GetExhibitionsUseCase,
 ) : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
@@ -22,22 +23,21 @@ class DashboardViewModel @Inject constructor(
     val text: LiveData<String> = _text
 
     var artWork = MutableLiveData<UIState<ExhibitionResponse>>(UIState.Loading())
+
     //var artWork = MutableStateFlow<UIState<ExhibitionResponse>>(UIState.Loading())
     init {
         getArtWorks()
     }
 
-    fun getArtWorks() {
+    private fun getArtWorks() {
         viewModelScope.launch {
+            //viewModelScope is a predefined CoroutineScope that is included with the ViewModel
+            //Dispatchers.IO indicates that this coroutine should be executed on a thread reserved for I/O operations.
             when (val response = getExhibitionsUseCase.invoke()) {
                 is UIState.Success -> artWork.postValue(UIState.Success(response.responseData))
                 is UIState.Error -> artWork.postValue(UIState.Error(response.error))
-                is UIState.Empty -> artWork.postValue(
-                    UIState.Empty(
-                        title = response.title,
-                    )
-                )
-                else -> {}
+                is UIState.Empty -> artWork.postValue(UIState.Empty(title = response.title))
+                is UIState.Loading -> artWork.postValue(UIState.Loading())
             }
         }
     }
